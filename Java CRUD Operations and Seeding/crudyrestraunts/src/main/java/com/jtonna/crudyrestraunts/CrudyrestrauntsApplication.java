2b29e50554d4637e75062d34fab9cdcbf45ae33e
Jacob Tonna <34466548+Jtonna@users.noreply.github.com>
2019-12-24 22:03:05 -0800

    Initialized spring boot application
    Initialized spring boot application with spring web, springdev tools, h2, spring data jpa.
    
    Currently having import issues and looking for solutions

---
+++ Java CRUD Operations and Seeding/crudyrestraunts/src/main/java/com/jtonna/crudyrestraunts/CrudyrestrauntsApplication.java
@@ -0,0 +1,13 @@
+package com.jtonna.crudyrestraunts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudyrestrauntsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CrudyrestrauntsApplication.class, args);
+    }
+
+}
