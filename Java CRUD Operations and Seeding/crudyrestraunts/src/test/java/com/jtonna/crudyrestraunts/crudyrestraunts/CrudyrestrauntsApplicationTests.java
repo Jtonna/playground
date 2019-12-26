bfb36dfc4afdb2866e0a9159fe15828bb641cff0
Jacob Tonna <34466548+Jtonna@users.noreply.github.com>
2019-12-26 15:29:39 -0800

    Re-created project & made changes
    Re-initialized project with
    spring web
    spring dev tools
    h2 database
    spring data jpa
    
    removed h2's scope from runtime
    added folders from controllers, models & repos

---
+++ Java CRUD Operations and Seeding/crudyrestraunts/src/test/java/com/jtonna/crudyrestraunts/crudyrestraunts/CrudyrestrauntsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jtonna.crudyrestraunts.crudyrestraunts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudyrestrauntsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
