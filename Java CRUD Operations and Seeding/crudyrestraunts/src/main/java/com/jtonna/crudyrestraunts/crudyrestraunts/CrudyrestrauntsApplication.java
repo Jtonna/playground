be9217f91e7862edcd2e79a71ef34c9a437b49a2
Jacob Tonna <34466548+Jtonna@users.noreply.github.com>
2019-12-27 14:14:15 -0800

    Re-initialized project
    Initialized project with spring web, spring dev tools, h2, spring data JPA
    
    commented out H2's runtime scope and created controllers, models repos and service packages

---
+++ Java CRUD Operations and Seeding/crudyrestraunts/src/main/java/com/jtonna/crudyrestraunts/crudyrestraunts/CrudyrestrauntsApplication.java
@@ -0,0 +1,13 @@
+package com.jtonna.crudyrestraunts.crudyrestraunts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudyrestrauntsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CrudyrestrauntsApplication.class, args);
+    }
+
+}
