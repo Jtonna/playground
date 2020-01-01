ef1b6fde03db6b75549ce921de0621b1268e3edb
Jacob Tonna <34466548+Jtonna@users.noreply.github.com>
2019-12-31 17:05:27 -0800

    Model Development
    Developing models for a restaurant, as well as the dish items it may serve. I keep spelling restaurant wrong and its driving me nuts.
    
    Going to work on the relationship's next

--- Java CRUD Operations and Seeding/crudrestaurants/src/main/java/com/jtonna/crudrestaurants/crudrestaurants/models/Menu.java
+++ Java CRUD Operations and Seeding/crudrestaurants/src/main/java/com/jtonna/crudrestaurants/crudrestaurants/models/Menu.java
@@ -1,4 +1,27 @@
 package com.jtonna.crudrestaurants.crudrestaurants.models;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name = "menus")
 public class Menu {
+
+    // Private ID primarily used for relationship mapping in join tables
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long menuid;
+
+    // fields for various values
+    @Column(unique = true, nullable = false)
+    private String dishname;
+
+    @Column(unique = true, nullable = false)
+    private String dishdescription;
+
+    @Column(unique = true, nullable = false)
+    private double dishprice;
+
+    // Relationship between Restaurant Model
+
+
 }
--- Java CRUD Operations and Seeding/crudrestaurants/src/main/java/com/jtonna/crudrestaurants/crudrestaurants/models/Restaurant.java
+++ Java CRUD Operations and Seeding/crudrestaurants/src/main/java/com/jtonna/crudrestaurants/crudrestaurants/models/Restaurant.java
@@ -1,5 +1,33 @@
 package com.jtonna.crudrestaurants.crudrestaurants.models;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name = "restaurants")
 public class Restaurant {
-    
+
+    // Generated ID primarily used for mapping
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long restaurantid;
+
+    // fields
+    @Column(unique = true, nullable = false)
+    private String name;
+
+    @Column(unique = true, nullable = false)
+    private String address;
+
+    @Column(unique = true, nullable = false)
+    private String city;
+
+    @Column(unique = true, nullable = false)
+    private String state;
+
+    @Column(unique = true, nullable = false)
+    private String telephone;
+
+    // Relationship between Menu Model
+
+
 }
