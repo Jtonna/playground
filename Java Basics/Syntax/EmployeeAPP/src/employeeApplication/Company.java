70b9e77941b55479e517f4f38bde52deabae5d85
Jacob Tonna <34466548+Jtonna@users.noreply.github.com>
2020-03-03 16:54:48 -0800

    Update Company.java

---
+++ Java Basics/Syntax/EmployeeAPP/src/employeeApplication/Company.java
@@ -0,0 +1,20 @@
+package employeeApplication; // name of the package
+
+public class Company
+{
+    // Fields
+    private static maxId = 0;
+    public final static double match401k = 0.05; // Final is basically a const in javascript, meaning it cant be changed & Static means all objects share it
+
+    public int id;
+    public String name;
+    public int debt;
+
+    // Constructor
+    public Company
+    {
+        this.id = id;
+        this.name = name;
+        this.debt = debt;
+    }
+}
