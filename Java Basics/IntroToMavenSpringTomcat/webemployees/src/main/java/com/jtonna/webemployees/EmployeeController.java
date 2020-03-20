f1f81dd63a30d3bb3aeadd2997c4bcd0ea56af13
Jacob Tonna <34466548+Jtonna@users.noreply.github.com>
2020-03-20 01:28:13 -0700

    Implemented finding employees by first letter of the name

--- Java Basics/IntroToMavenSpringTomcat/webemployees/src/main/java/com/jtonna/webemployees/EmployeeController.java
+++ Java Basics/IntroToMavenSpringTomcat/webemployees/src/main/java/com/jtonna/webemployees/EmployeeController.java
@@ -4,6 +4,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
+
 @RestController
 @RequestMapping(value = "/data")
 public class EmployeeController
@@ -24,5 +26,14 @@ public class EmployeeController
     {
         Employee rtnEmp = WebemployeesApplication.ourEmpList.findEmployee(e -> (e.getId() == id));
         return new ResponseEntity<>(rtnEmp, HttpStatus.OK);
+    }
+
+    // localhost:PORT/data/employees/<letter of fname>
+    @GetMapping(value = "/employees/{letter}", produces = {"application/json"})
+    public ResponseEntity<?> getEmployees(@PathVariable char letter)
+    {
+        ArrayList<Employee> rtnEmps = WebemployeesApplication.ourEmpList.
+                findEmployees( e -> e.getFname().toUpperCase().charAt(0) == Character.toUpperCase(letter));
+        return new ResponseEntity<>(rtnEmps, HttpStatus.OK);
     }
 }
