9156309f236b39b03ba50a900fbad2d2299e94dd
Jacob Tonna <34466548+Jtonna@users.noreply.github.com>
2020-03-23 16:37:43 -0700

    Formatted Main & Created Animal interface

--- Java Projects/transport/src/com/jtonna/Main.java
+++ Java Projects/transport/src/com/jtonna/Main.java
@@ -1,8 +1,10 @@
 package com.jtonna;
 
-public class Main {
+public class Main
+{
 
-    public static void main(String[] args) {
-	// write your code here
+    public static void main(String[] args)
+    {
+        // write your code here
     }
 }
