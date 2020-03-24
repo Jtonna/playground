f4a6df7ec4f2346d23096aeb0a2610138cd573d6
Jacob Tonna <34466548+Jtonna@users.noreply.github.com>
2020-03-23 20:29:45 -0700

    Created and setup Horse.java with implemented interfaces

---
+++ Java Projects/transport/src/com/jtonna/Horse.java
@@ -0,0 +1,64 @@
+package com.jtonna;
+
+public class Horse implements Vehicle, Animal
+{
+    private int fuel = 0;
+    private String name;
+
+    // Constructor
+    public Horse(String name)
+    {
+        this.name = name;
+    }
+
+    // Get & set names
+    public String getName()
+    {
+        return name;
+    }
+
+    public void setName(String name)
+    {
+        this.name = name;
+    }
+
+    // Overrides for Animal Interface
+    @Override
+    public void eat(int i)
+    {
+        // Set fuel levels
+        fuel = fuel + i;
+    }
+
+    @Override
+    public String speak()
+    {
+        // What does the horse say
+        return "Neigh";
+    }
+
+    // Overrides for Vehicle Interface
+    @Override
+    public String getPath()
+    {
+        return "Grass";
+    }
+
+    @Override
+    public void move()
+    {
+        fuel = fuel - 1;
+    }
+
+    @Override
+    public int getFuelLevel()
+    {
+        return fuel;
+    }
+
+    @Override
+    public void addFuel(int i)
+    {
+        eat(i);
+    }
+}
