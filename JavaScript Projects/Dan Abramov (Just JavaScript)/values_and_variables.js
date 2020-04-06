b7f1d22f73039648e3f7911de178054050600476
Jacob Tonna <34466548+Jtonna@users.noreply.github.com>
2020-04-05 18:07:52 -0700

    Immutability with primitive values

--- JavaScript Projects/Dan Abramov (Just JavaScript)/values_and_variables.js
+++ JavaScript Projects/Dan Abramov (Just JavaScript)/values_and_variables.js
@@ -21,5 +21,41 @@ let review_example_3 = typeof(typeof(example_val_3))
 console.log(review_example_3) // string?
 
 /*
-    Review is over, time to get to some work.
+    Kicking off the module with the following code snippet
+    Dan asks us to take a few moments and write down what we expect to happen line by line
 */
+
+let reaction = 'yikes'; // declare a variable named "reaction" that contains a string "yikes"
+reaction[0] = 'l'; // set reaction @ index 0 "y" to "l"
+console.log(reaction); // console log reaction "likes"
+
+// The code will either print "yikes" or throw an error depending on whether we are in "strict mode"
+// It will NEVER print "likes"
+// Primitive values are Immutable, meaning they cant be modified
+
+// Explained with a small example
+
+// Below we have an array (which is mutable) and a string (immutable)
+let arr = [212, 8, 506];
+let str = 'hello';
+
+// Here we are accessing the first item in both the array & the string, we use the same syntax but dont get confused
+// unlike python strings ARE NOT arrays
+console.log(arr[0]) // 212
+console.log(str[0]) // "h"
+
+// Lets change the arrays first item
+arr[0] = 420;
+console.log(arr) // [420, 8, 506]
+
+// As demonstrated with the code snippet dan gave us, we cant do the same to Strings, because they are immutable.
+// This means "unchangeable" in latin. To be more specific its not just Strings; its any primitive value thats off limits.
+
+let fifty = 50
+fifty.shades = 'gray'
+
+// Like any number 50 is a primitive value, so we cant set properties on it. Its off limits.
+
+// In dans JavaScript mental model, all primitive values exist in the outer circle further from his code, like distant star's.
+// he can refer to them and create new ones, but he cant change them, they stay what they are.
+
