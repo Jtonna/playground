6ce7ece4f3d13ae92f1c4c146bd00a496f61acfe
Jacob Tonna <34466548+Jtonna@users.noreply.github.com>
2020-04-11 02:53:02 -0700

    Properly counting number of lines from shell & the true number of processes on system

--- Python Projects/Adobe Killer/get_tasks.py
+++ Python Projects/Adobe Killer/get_tasks.py
@@ -14,21 +14,22 @@ def get_tasks():
     # https://docs.python.org/3/library/subprocess.html#module-subprocess
     processes_list = subprocess.Popen(cmd_get_tasks, shell=True, universal_newlines=True, stdout=subprocess.PIPE)
 
-    # For each process in the shell add 1 to num_of_processes
-    num_of_processes = 0
-    num_of_not = 0
+    # For each individual "process" line in the "shell" we spawned
+    num_of_lines_from_shell = int()
+    num_of_processes = int()
     for process in processes_list.stdout:
 
         # Increment num_of_processes (used for index in the dict)
-        num_of_processes = num_of_processes +1
+        num_of_lines_from_shell = num_of_lines_from_shell +1
 
-        # Each "process" line we care about should be => 1062
-        if len(process) > 1062: # Filter out empry spaces
-            print(process)
+        # Each "process" has a line length of 1070 for some reason, we use this to filter out lines that are irrelevant
+        if len(process) == 1070:
+            num_of_processes = num_of_processes + 1
+
             
     
+    print(f"num_of_lines_from_shell = {num_of_lines_from_shell}")
     print(f"num_of_processes = {num_of_processes}")
-    print(num_of_not)
         
 
 get_tasks()  
