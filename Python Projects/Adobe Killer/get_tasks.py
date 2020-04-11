99bfa4ba54b3031dc5d7cab7197e4017b2b003b0
Jacob Tonna <34466548+Jtonna@users.noreply.github.com>
2020-04-11 04:01:39 -0700

    Properly filtering out adobe_killer from the proccesses

--- Python Projects/Adobe Killer/get_tasks.py
+++ Python Projects/Adobe Killer/get_tasks.py
@@ -4,7 +4,7 @@ import subprocess
 kill_list = dict()
 
 # Gets running tasks on the windows machine in order of ProcessID, Caption (name), CommandLine (location of file)
-def get_tasks():
+def get_the_tasks():
 
     # We are using the WMIC command followed by the PROCESS GET x, x, x command to get a list of everything running on the machine
     # http://net-informations.com/q/mis/wmic.html
@@ -27,23 +27,32 @@ def get_tasks():
         if len(process) == 1070:
             num_of_processes = num_of_processes + 1
 
-            # If the "process" contains the name "Adobe Killer", we continue since we dont want to add this process or related ones to the kill list
+            # Filter our program out so we dont prematurely kill it
             if "Adobe Killer" in process:
                 continue
 
+            # Filter our program out so we dont prematurely kill it
+            if "Adobe_Killer" in process:
+                continue
+
             # If the current process contains "Adobe"
-            if "Adobe" in process:
+            if process.find("Adobe") != -1:
                 num_of_adobe_processes = num_of_adobe_processes + 1
                 print(process)
+
+                # Add the Caption "process name" to the kill list dictionary
+
+                
 
             
     
     print(f"num_of_lines_from_shell = {num_of_lines_from_shell}")
     print(f"num_of_processes = {num_of_processes}")
     print(f"num_of_adobe_proc = {num_of_adobe_processes}")
+    print(kill_list)
         
 
-get_tasks()  
+get_the_tasks()  
     
 # processes = {
 #     1:{
