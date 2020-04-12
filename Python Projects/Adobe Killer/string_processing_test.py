8d74ce420e6147cebeaba7e186f42a7be049f38e
Jacob Tonna <34466548+Jtonna@users.noreply.github.com>
2020-04-12 04:28:13 -0700

    Everything's fucked, starting over because some Windows Processes dont have a file location LOL
    I hate windows! If a process is "spawned" by another process its file location should be that of its parent process. What the fuck!

--- Python Projects/Adobe Killer/string_processing_test.py
+++ Python Projects/Adobe Killer/string_processing_test.py
@@ -1,49 +1,19 @@
-process = "Adobe_XD                                              'C:\ewerwffe\efew\dobe\efwfewfdf324 -args -moreargs'                     426462"
+process = 'AdobeNotificationClient.exe                                         "C:\Program Files\WindowsApps\AdobeNotificationClient_1.0.1.22_x86__enpm4xejd91yc\AdobeNotificationClient.exe" -ServerName:App.AppXbdz14xebceycqvrazxqtnx89wn9e0ebz.mca                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                       17824'
 
 # Take in a start & end index for a string
 def substring_constructor(substring_start_index, substring_end_index):
     print("\n*Substring Constructor")
     print(f"start & end index for a string {substring_start_index, substring_end_index}\n")
 
-string_end_index = None
-string_start_index = None
-# Loop over len(process) number of times
+# main string refer's to "process" which is a string that is 1070 characters long and riddle'd with spaces and 3 sub-strings that are a Program Name, Program Drive Location & Process Id (PID)
 for i in range(len(process)):
-    last_index = len(process) - 1
-    current_index = i
-    current_index_char = process[i]
-    print(f"\n{current_index_char} ||||| Loop number {i} || string_start_index {string_start_index} || string_end_index {string_end_index} || strings found {strings_found}")
+    print(f"\n{process[i]}    |||  loop #{i} of {len(process)-1}")
 
-    # If we are at the last_index we have found the end of the last string
-    if current_index == last_index:
-        string_end_index = last_index
-        print(f"*Last Index last index {last_index}")
-        print(f"    Setting string_end_index {string_end_index} which should be the same as last index")
-        pass # continue the rest of the code
-
-    # If we have an end index we should have a start index too, so we pass them to the substring_constructor and reset values
-    if string_end_index is not None:
-        strings_found = strings_found + 1
-        substring_constructor(string_start_index, string_end_index)
-        # We also want to reset the value's for the nest string if theres going to be another one
-        continue # start a new loop if applicable
-
-    # Move through groups of spaces & ignore args for files
-    if current_index_char == ' ':
-            if process[i+1] == ' ':
-                continue # start another loop
-
-    # Identify the start of a new stirng
-    if current_index_char != ' ':
-        if process[i+1] != ' ':
-            if string_start_index == None:
-                string_start_index = current_index
-                print(f"    Found the start of a string at {current_index} with a character of {current_index_char}")
-                
-    # Identify the end of a sub-string
-    if current_index_char == ' ':
-            if process[i+1] != '-': # move past program args that contain a space followed by -
-                print(f"FOUND THE END OF A SUB-STRING AT {current_index}")
-                print(f"    the actual index of the sub-string is {current_index-1}")
+    # Move through an empty space in the main string then start the loop again
+    if process[i] == ' ':
+        if process[i+1] == ' ':
+            continue
     
-    print(current_index_char)
+    # Now all "sub-strings" should be seperated by a single space
+    print(process[i])
+
