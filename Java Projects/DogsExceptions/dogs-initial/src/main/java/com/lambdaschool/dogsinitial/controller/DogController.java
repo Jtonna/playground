9671eb57a5bbc239dfb58b7cb9e51c12d9eeec1b
Jacob Tonna <34466548+Jtonna@users.noreply.github.com>
2020-05-10 21:09:59 -0700

    setup produces for cleaner auto documentation

--- Java Projects/DogsExceptions/dogs-initial/src/main/java/com/lambdaschool/dogsinitial/controller/DogController.java
+++ Java Projects/DogsExceptions/dogs-initial/src/main/java/com/lambdaschool/dogsinitial/controller/DogController.java
@@ -5,10 +5,7 @@ import com.lambdaschool.dogsinitial.model.Dog;
 import com.lambdaschool.dogsinitial.DogsinitialApplication;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
 import java.util.ArrayList;
@@ -18,27 +15,30 @@ import java.util.ArrayList;
 public class DogController
 {
     // localhost:8080/dogs/dogs
-    @GetMapping(value = "/dogs")
+    @GetMapping(value = "/dogs", produces = {"application/json"})
     public ResponseEntity<?> getAllDogs()
     {
         return new ResponseEntity<>(DogsinitialApplication.ourDogList.dogList, HttpStatus.OK);
     }
 
     // localhost:8080/dogs/{id}
-    @GetMapping(value = "/{id}")
+    @GetMapping(value = "/{id}", produces = {"application/json"})
     public ResponseEntity<?> getDogDetail(@PathVariable long id)
     {
         Dog rtnDog;
-        if(DogsinitialApplication.ourDogList.findDog(d -> (d.getId() == id))== null){
-            throw new ResourceNotFoundException("The dog with the id "+ id + " does not exist");
-        } else{
+        System.out.println(id);
+
+        if(DogsinitialApplication.ourDogList.findDog(d -> (d.getId() == id))== null)
+        {
+            throw new ResourceNotFoundException("The dog with the id " + id + " does not exist");
+        } else {
             rtnDog = DogsinitialApplication.ourDogList.findDog(d -> (d.getId() == id));
         }
         return new ResponseEntity<>(rtnDog, HttpStatus.OK);
     }
 
     // localhost:8080/dogs/breeds/{breed}
-    @GetMapping(value = "/breeds/{breed}")
+    @GetMapping(value = "/breeds/{breed}", produces = {"application/json"})
     public ResponseEntity<?> getDogBreeds (@PathVariable String breed)
     {
         ArrayList<Dog> rtnDogs = DogsinitialApplication.ourDogList.
@@ -47,7 +47,7 @@ public class DogController
     }
 
     // localhost:2019/dogs/dogtable
-    @GetMapping(value = "/dogtable")
+    @GetMapping(value = "/dogtable", produces = {"application/json"})
     public ModelAndView showDogTable(){
         ModelAndView mav = new ModelAndView();
         mav.setViewName("dogs");
