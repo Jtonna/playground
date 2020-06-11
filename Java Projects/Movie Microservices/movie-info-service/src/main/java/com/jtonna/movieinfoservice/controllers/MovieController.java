8cb1185eee9bb8984602ce583566cc23f63f2afa
Jacob Tonna <34466548+Jtonna@users.noreply.github.com>
2020-06-11 16:37:56 -0700

    fixed path variable bug

--- Java Projects/Movie Microservices/movie-info-service/src/main/java/com/jtonna/movieinfoservice/controllers/MovieController.java
+++ Java Projects/Movie Microservices/movie-info-service/src/main/java/com/jtonna/movieinfoservice/controllers/MovieController.java
@@ -1,8 +1,8 @@
 package com.jtonna.movieinfoservice.controllers;
 
 import com.jtonna.movieinfoservice.models.Movie;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController()
@@ -11,7 +11,7 @@ public class MovieController
 {
 
     @RequestMapping("/{movieId}")
-    public Movie getMovieInfo(@RequestParam("movieId") String movieId)
+    public Movie getMovieInfo(@PathVariable("movieId") String movieId)
     {
         return new Movie(
                 movieId,
