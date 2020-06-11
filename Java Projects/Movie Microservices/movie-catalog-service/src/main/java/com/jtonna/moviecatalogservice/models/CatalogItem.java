fd04a73f28708f14faffcadd8239020d5f1af074
Jacob Tonna <34466548+Jtonna@users.noreply.github.com>
2020-06-11 15:55:18 -0700

    Created Catalog item model

---
+++ Java Projects/Movie Microservices/movie-catalog-service/src/main/java/com/jtonna/moviecatalogservice/models/CatalogItem.java
@@ -0,0 +1,45 @@
+package com.jtonna.moviecatalogservice.models;
+
+public class CatalogItem
+{
+
+    private String name, description;
+    private int rating;
+
+    public CatalogItem(String name, String description, int rating)
+    {
+        this.name = name;
+        this.description = description;
+        this.rating = rating;
+    }
+
+    public String getName()
+    {
+        return name;
+    }
+
+    public void setName(String name)
+    {
+        this.name = name;
+    }
+
+    public String getDescription()
+    {
+        return description;
+    }
+
+    public void setDescription(String description)
+    {
+        this.description = description;
+    }
+
+    public int getRating()
+    {
+        return rating;
+    }
+
+    public void setRating(int rating)
+    {
+        this.rating = rating;
+    }
+}
