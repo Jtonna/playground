fdf25ceefc6d575f3fa30cf2c5fa2ec8c3bc61e4
Jacob Tonna <34466548+Jtonna@users.noreply.github.com>
2020-06-11 20:44:40 -0700

    Movie Microservice - added models for movie and rating & spoofing async call
    Added Moving & Rating Models to main microservice & fixed the restTemplate to use a bean instead of create and discard one every time

--- Java Projects/Movie Microservices/movie-catalog-service/src/main/java/com/jtonna/moviecatalogservice/controllers/MovieCatalogController.java
+++ Java Projects/Movie Microservices/movie-catalog-service/src/main/java/com/jtonna/moviecatalogservice/controllers/MovieCatalogController.java
@@ -1,34 +1,44 @@
 package com.jtonna.moviecatalogservice.controllers;
 
 import com.jtonna.moviecatalogservice.models.CatalogItem;
+import com.jtonna.moviecatalogservice.models.Movie;
+import com.jtonna.moviecatalogservice.models.Rating;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/catalog")
 public class MovieCatalogController
 {
 
+    @Autowired
+    private RestTemplate restTemplate;
+
     @RequestMapping("/{movieId}")
     public List<CatalogItem> getCatalog(@PathVariable("movieId") String movieId)
     {
         // get all rated movie id's
+        List<Rating> ratings = Arrays.asList(
+                new Rating ("1234", 4),
+                new Rating("5679", 2)
+        );
 
         /// for each movie id, call movie info service and get details
+        return ratings.stream().map(rating -> {
+            // System.out.println("api call");
+            Movie movie = restTemplate.getForObject("http://localhost:8082/movies/" + rating.getMovieId(), Movie.class);
+            return new CatalogItem(movie.getName(),"this movie is about....", rating.getRating());
+        }).collect(Collectors.toList());
 
         // put them together to return the info
 
-
-        return Collections.singletonList(
-                new CatalogItem(
-                        "Pixels",
-                        "When aliens misinterpret video feeds of classic arcade games as a declaration of war against them, they attack the Earth using games like PAC-MAN, Donkey Kong, Galaga, Centipede and Space Invaders as…",
-                        52
-                )
-        );
     }
 }
---
+++ Java Projects/Movie Microservices/movie-catalog-service/src/main/java/com/jtonna/moviecatalogservice/models/Movie.java
@@ -0,0 +1,34 @@
+package com.jtonna.moviecatalogservice.models;
+
+public class Movie
+{
+    private String movieId, name;
+
+    public Movie(){}
+
+    public Movie(String movieId, String name)
+    {
+        this.movieId = movieId;
+        this.name = name;
+    }
+
+    public String getMovieId()
+    {
+        return movieId;
+    }
+
+    public void setMovieId(String movieId)
+    {
+        this.movieId = movieId;
+    }
+
+    public String getName()
+    {
+        return name;
+    }
+
+    public void setName(String name)
+    {
+        this.name = name;
+    }
+}
