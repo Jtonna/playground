df0af6cbfd6de7940fa00f01a2173b000c3f424e
Jacob Tonna <34466548+Jtonna@users.noreply.github.com>
2020-06-12 02:23:56 -0700

    explored using WebClient.builder for the future since restTemplate is due to be deprecated

--- Java Projects/Movie Microservices/movie-catalog-service/src/main/java/com/jtonna/moviecatalogservice/controllers/MovieCatalogController.java
+++ Java Projects/Movie Microservices/movie-catalog-service/src/main/java/com/jtonna/moviecatalogservice/controllers/MovieCatalogController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
 
 import java.util.Arrays;
 import java.util.Collections;
@@ -22,9 +23,13 @@ public class MovieCatalogController
     @Autowired
     private RestTemplate restTemplate;
 
+    @Autowired
+    private WebClient.Builder webClientBuilder;
+
     @RequestMapping("/{movieId}")
     public List<CatalogItem> getCatalog(@PathVariable("movieId") String movieId)
     {
+
         // get all rated movie id's
         List<Rating> ratings = Arrays.asList(
                 new Rating ("1234", 4),
@@ -32,9 +37,18 @@ public class MovieCatalogController
         );
 
         /// for each movie id, call movie info service and get details
+
         return ratings.stream().map(rating -> {
-            // System.out.println("api call");
-            Movie movie = restTemplate.getForObject("http://localhost:8082/movies/" + rating.getMovieId(), Movie.class);
+        Movie movie = restTemplate.getForObject("http://localhost:8082/movies/" + rating.getMovieId(), Movie.class);
+
+//        Below is an example of how we should be doing things in the future because restTemplate is due to become deprecated
+//        Movie movie = WebClient.builder().build()
+//                .get()
+//                .uri("http://localhost:8082/movies/" + rating.getMovieId())
+//                .retrieve()
+//                .bodyToMono(Movie.class)
+//                .block();
+
             return new CatalogItem(movie.getName(),"this movie is about....", rating.getRating());
         }).collect(Collectors.toList());
 
