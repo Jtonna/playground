5afba79f1f10fc64236be675c6b05ee1ee0a4c0a
Jacob Tonna <34466548+Jtonna@users.noreply.github.com>
2020-06-13 16:37:24 -0700

    Calling two apis and parsing/returning data now

--- Java Projects/Movie Microservices/movie-catalog-service/src/main/java/com/jtonna/moviecatalogservice/controllers/MovieCatalogController.java
+++ Java Projects/Movie Microservices/movie-catalog-service/src/main/java/com/jtonna/moviecatalogservice/controllers/MovieCatalogController.java
@@ -3,6 +3,7 @@ package com.jtonna.moviecatalogservice.controllers;
 import com.jtonna.moviecatalogservice.models.CatalogItem;
 import com.jtonna.moviecatalogservice.models.Movie;
 import com.jtonna.moviecatalogservice.models.Rating;
+import com.jtonna.moviecatalogservice.models.UserRating;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -26,20 +27,24 @@ public class MovieCatalogController
     @Autowired
     private WebClient.Builder webClientBuilder;
 
-    @RequestMapping("/{movieId}")
-    public List<CatalogItem> getCatalog(@PathVariable("movieId") String movieId)
+    @RequestMapping("/{userId}")
+    public List<CatalogItem> getCatalog(@PathVariable("userId") String userId)
     {
+        UserRating ratings = restTemplate.getForObject("http://localhost:8083/ratingsdata/users/" + userId, UserRating.class);
 
-        // get all rated movie id's
-        List<Rating> ratings = Arrays.asList(
-                new Rating ("1234", 4),
-                new Rating("5679", 2)
-        );
+
+        return ratings.getUserRating().stream().map(rating -> {
 
         /// for each movie id, call movie info service and get details
-
-        return ratings.stream().map(rating -> {
         Movie movie = restTemplate.getForObject("http://localhost:8082/movies/" + rating.getMovieId(), Movie.class);
+
+        // put them together to return the info
+            return new CatalogItem(movie.getName(),"this movie is about....", rating.getRating());
+        }).collect(Collectors.toList());
+        
+    }
+}
+
 
 //        Below is an example of how we should be doing things in the future because restTemplate is due to become deprecated
 //        Movie movie = WebClient.builder().build()
@@ -48,11 +53,3 @@ public class MovieCatalogController
 //                .retrieve()
 //                .bodyToMono(Movie.class)
 //                .block();
-
-            return new CatalogItem(movie.getName(),"this movie is about....", rating.getRating());
-        }).collect(Collectors.toList());
-
-        // put them together to return the info
-
-    }
-}
--- Java Projects/Movie Microservices/movie-ratings-service/src/main/java/com/jtonna/movieratingsservice/controllers/RatingsController.java
+++ Java Projects/Movie Microservices/movie-ratings-service/src/main/java/com/jtonna/movieratingsservice/controllers/RatingsController.java
@@ -1,9 +1,13 @@
 package com.jtonna.movieratingsservice.controllers;
 
 import com.jtonna.movieratingsservice.models.Rating;
+import com.jtonna.movieratingsservice.models.UserRating;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.List;
 
 @RestController
 @RequestMapping("/ratingsdata")
@@ -14,5 +18,20 @@ public class RatingsController
     public Rating getRating(@PathVariable("movieId") String movieId)
     {
         return new Rating(movieId, 4);
+    }
+
+    @RequestMapping("/users/{userId}")
+    public UserRating getUserRating(@PathVariable("userId") String userId)
+    {
+        // get all rated movie id's
+        List<Rating> ratings = Arrays.asList(
+                new Rating("1234", 4),
+                new Rating("5679", 2)
+        );
+
+        UserRating userRating = new UserRating();
+        userRating.setUserRating(ratings);
+
+        return userRating;
     }
 }
