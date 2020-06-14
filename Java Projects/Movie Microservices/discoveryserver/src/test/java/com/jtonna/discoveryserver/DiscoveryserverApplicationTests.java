165f5d41c65314cf73e53a7cfbc8c33f07768bd4
Jacob Tonna <34466548+Jtonna@users.noreply.github.com>
2020-06-14 03:24:10 -0700

    Created Eureka Discovery Server
    Created Eureka Discovery Server, added jaxb dependencies & configured application.properties

---
+++ Java Projects/Movie Microservices/discoveryserver/src/test/java/com/jtonna/discoveryserver/DiscoveryserverApplicationTests.java
@@ -0,0 +1,15 @@
+package com.jtonna.discoveryserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryserverApplicationTests
+{
+
+    @Test
+    void contextLoads()
+    {
+    }
+
+}
