80c1f4dea6e5ce4edb6ea6d0a87bf9743a27b3c0
Jacob Tonna <34466548+Jtonna@users.noreply.github.com>
2020-06-19 01:34:46 -0700

    Finished the payments model

--- Java Projects/JX30 LS/crudyrestaurants/src/main/java/com/jtonna/crudyrestaurants/models/Payment.java
+++ Java Projects/JX30 LS/crudyrestaurants/src/main/java/com/jtonna/crudyrestaurants/models/Payment.java
@@ -17,4 +17,49 @@ public class Payment
 
     @ManyToMany(mappedBy = "payments")
     private List<Restaurant> restaurants = new ArrayList<>();
+
+    public Payment()
+    {
+    }
+
+    public Payment(String type)
+    {
+        this.type = type;
+    }
+
+    public long getPaymentid()
+    {
+        return paymentid;
+    }
+
+    public void setPaymentid(long paymentid)
+    {
+        this.paymentid = paymentid;
+    }
+
+    public String getType()
+    {
+        return type;
+    }
+
+    public void setType(String type)
+    {
+        this.type = type;
+    }
+
+    public List<Restaurant> getRestaurants()
+    {
+        return restaurants;
+    }
+
+    public void setRestaurants(List<Restaurant> restaurants)
+    {
+        this.restaurants = restaurants;
+    }
+
+    @Override
+    public String toString()
+    {
+        return "Payment{" + "paymentid=" + paymentid + ", type='" + type + '\'' + ", restaurants=" + restaurants + '}';
+    }
 }
