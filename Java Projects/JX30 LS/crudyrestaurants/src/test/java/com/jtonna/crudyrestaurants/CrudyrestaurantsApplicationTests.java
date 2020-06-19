2c8ee3c6315a9dc49b16a67dec90fcd31b7ef67a
Jacob Tonna <34466548+Jtonna@users.noreply.github.com>
2020-06-18 18:17:56 -0700

    Init crudy rest

---
+++ Java Projects/JX30 LS/crudyrestaurants/src/test/java/com/jtonna/crudyrestaurants/CrudyrestaurantsApplicationTests.java
@@ -0,0 +1,15 @@
+package com.jtonna.crudyrestaurants;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudyrestaurantsApplicationTests
+{
+
+    @Test
+    void contextLoads()
+    {
+    }
+
+}
