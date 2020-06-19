46839968ba9c89e8d15cce75b4194d860a6f13bc
Jacob Tonna <34466548+Jtonna@users.noreply.github.com>
2020-06-19 16:12:05 -0700

    Setup RestaurantServiceImpl

---
+++ Java Projects/JX30 LS/crudyrestaurants/src/main/java/com/jtonna/crudyrestaurants/services/RestaurantServiceImpl.java
@@ -0,0 +1,27 @@
+package com.jtonna.crudyrestaurants.services;
+
+import com.jtonna.crudyrestaurants.models.Restaurant;
+
+import java.util.List;
+
+// Generate Override Methods from the RestaurantService
+public class RestaurantServiceImpl implements RestaurantService
+{
+    @Override
+    public List<Restaurant> findAllRestaurants()
+    {
+        return null;
+    }
+
+    @Override
+    public Restaurant findRestaurantById(long id)
+    {
+        return null;
+    }
+
+    @Override
+    public List<Restaurant> findByNameLike()
+    {
+        return null;
+    }
+}
