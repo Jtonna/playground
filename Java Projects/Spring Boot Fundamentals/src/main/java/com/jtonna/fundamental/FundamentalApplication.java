ab7a368fe6c145652fbbb6b44168d34f09052b22
Jacob Tonna <34466548+Jtonna@users.noreply.github.com>
2020-08-15 16:46:12 -0700

    Update FundamentalApplication.java

--- Java Projects/Spring Boot Fundamentals/src/main/java/com/jtonna/fundamental/FundamentalApplication.java
+++ Java Projects/Spring Boot Fundamentals/src/main/java/com/jtonna/fundamental/FundamentalApplication.java
@@ -5,9 +5,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class FundamentalApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(FundamentalApplication.class, args);
+        System.out.println("Hello Jacob");
     }
 
 }
