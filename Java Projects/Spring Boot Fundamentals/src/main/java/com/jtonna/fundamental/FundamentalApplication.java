40b4c3e4795cc6af654fec9d1f6339c2cf108335
Jacob Tonna <34466548+Jtonna@users.noreply.github.com>
2020-08-15 16:41:08 -0700

    Init Project

---
+++ Java Projects/Spring Boot Fundamentals/src/main/java/com/jtonna/fundamental/FundamentalApplication.java
@@ -0,0 +1,13 @@
+package com.jtonna.fundamental;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FundamentalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FundamentalApplication.class, args);
+    }
+
+}
